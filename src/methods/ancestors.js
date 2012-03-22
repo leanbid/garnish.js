56d8383d363b9c21d791c4fea40f2fce214c5c2b
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-22 21:20:57 +0000

    refactored count() method

--- src/methods/ancestors.js
+++ src/methods/ancestors.js
@@ -1,5 +1,5 @@
 
-def('ancestors', function(){
+define_element_list_wrapper_method('ancestors', function(){
   var args = map_args(arguments, {filter: "true", include_this: false}, [
     [['string'], ['filter']],
     [['boolean'], ['include_this']],
--- src/methods/count.js
+++ src/methods/count.js
@@ -1,5 +1,5 @@
 
-def('count', function(){
-  return this.element.length;
+define_element_list_wrapper_method('count', function(){
+  return this.element_list.length;
 });
 
