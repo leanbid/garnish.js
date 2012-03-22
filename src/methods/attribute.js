16a71848717394dfb3f7dea26f7eca4a4c6140c6
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-22 21:08:29 +0000

    refactored attribute() method

--- src/methods/attribute.js
+++ src/methods/attribute.js
@@ -1,4 +1,4 @@
 
 def('attribute', function(name){
-  return this.is_attribute(name) ? this.element_list[0].attributes[name].value : arguments[1];
+  return this.is_attribute(name) ? this.element.attributes[name].value : arguments[1];
 });
