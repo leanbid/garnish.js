365db1f066c8c7caa3a1926911579e4c52e4165a
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-22 21:21:36 +0000

    refactored def() method

--- src/methods/def.js
+++ src/methods/def.js
@@ -1,14 +1,5 @@
 
 def('def', function(name, fn){
-  this.each(function(){
-    var shared_vars = this.shared_vars();
-    if(!is_defined(shared_vars.methods)){
-      shared_vars.methods = {};
-    }
-    shared_vars.methods[name] = fn;
-    this[name] = fn;
-  });
-  
   var shared_vars = this.shared_vars();
   if(!is_defined(shared_vars.methods)){
     shared_vars.methods = {};
