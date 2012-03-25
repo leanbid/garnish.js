fcf6f164304e5939484953a78979e4a04ccfa145
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-25 12:27:52 +0100

    fixed bind

--- src/methods/bind.js
+++ src/methods/bind.js
@@ -1,19 +1,17 @@
 
 def('bind', function(name, fn){
-  this.each(function(){
-    var that = this;
-    
-    function fn_wrapper(){
-      fn.apply(that, arguments);
-    }
-    
-    if(is_defined(this.element.addEventListener)){
-      this.element.addEventListener(name, fn_wrapper, false);
-    } else if(is_defined(this.element.attachEvent)){
-      this.element.attachEvent("on" + name, fn_wrapper);
-    }
-  });
+  var that = this;
   
+  function fn_wrapper(){
+    fn.apply(that, arguments);
+  }
+  
+  if(is_defined(this.element.addEventListener)){
+    this.element.addEventListener(name, fn_wrapper, false);
+  } else if(is_defined(this.element.attachEvent)){
+    this.element.attachEvent("on" + name, fn_wrapper);
+  }
+
   return this;
 });
 
