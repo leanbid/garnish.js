81ce3415ed5cd41101676076444189cb3fe86219
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-27 20:21:40 +0100

    removed old tests and created demos

--- tests/methods/init_ajax/test.php
+++
@@ -1,9 +0,0 @@
-
-<div>
-  State: <? echo isset($_GET['state']) ? $_GET['state'] : ''; ?>  
-</div>
-
-<input type="button" value="Foo" garnish="this.click(function(){this.ajax().get({state: 'foo'})})" />
-<input type="button" value="Bar" garnish="this.click(function(){this.ajax().get({state: 'bar'})})" />
-
-
--- tests/methods/init_dialog/test.php
+++
@@ -1,7 +0,0 @@
-
-<div>
-<p>This is inside an ajax_frame</p>
-
-<input type="button" value="close" garnish="this.click(function(){this.dialog().close();});" /> 
-</div>
-
