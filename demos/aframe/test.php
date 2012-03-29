723525ea5d8225c06f65b677f37dd1112e8f783b
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-03-29 07:49:57 +0100

    created a_tag initializer

---
+++ demos/aframe/test.php
@@ -0,0 +1,10 @@
+
+<p>Current Url: <?php echo $_SERVER['REQUEST_URI'] ?></p> 
+
+<p>Click on a link below:</p>
+
+<ul>
+  <li><a href="test.php?fruit=Apple">Apple</a></li>
+  <li><a href="test.php?fruit=Pear">Pear</a></li>
+  <li><a href="test.php?fruit=Plum">Plum</a></li>
+</ul>
---
+++ src/initializers/a_tag.js
@@ -0,0 +1,21 @@
+
+add_initializer(function(){
+  if(!this.is_type('a')){
+    return;
+  }
+  
+  var href = this.attribute('href');
+  if(!is_defined(href)){
+    return;
+  }
+  
+  var aframe = this.aframe();
+  if(!is_defined(aframe)){
+    return;
+  }
+
+  this.click(function(){
+    aframe.get(href);
+  });
+  
+});
