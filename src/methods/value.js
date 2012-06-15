d5b99696a7f1f8f55481bcc3016a8ee90a4b6497
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-06-15 07:33:27 +0100

    fixed bug with value()

--- src/methods/value.js
+++ src/methods/value.js
@@ -1,5 +1,6 @@
 
 def('value', function(){
+  var value;
   if(this.is_type('input') && this.is_attribute('type') && this.is_attribute('value') && (this.attribute('type')  == 'checkbox' || this.attribute('type') == 'radio')){
     if(this.property('checked')){
       value = this.attribute('value');
